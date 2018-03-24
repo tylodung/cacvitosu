de012793b56515c8560dbbb06f8b4b21c23d509e
Administrator <thaoamtanuyen@gmail.com>
2018-03-24 16:45:01 +0700

    up

--- src/html.js
+++ src/html.js
@@ -68,7 +68,7 @@ export default class HTML extends Component {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-8187753641663453',
+    google_ad_client: 'ca-pub-9416341948072738',
     enable_page_level_ads: true
   });
   `;
